16980f9f7ff061331ce6339802512fa1020b5361
ubol12341 <ubol1234.1@gmail.com>
2025-07-24 22:33:27 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,9 @@
-# killall com.termux
-# pkill -15 -u $(id -un)
-# pkill tech.ula
+am start -n com.termux/com.termux.app.TermuxActivity
+
+sleep 5s
+
+am start -n com.anydesk.anydeskandroid/com.anydesk.anydeskandroid.gui.activity.MainActivity
+
+sleep 5s
+
+am start -n com.termux/com.termux.app.TermuxActivity
