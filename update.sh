5401542ffa05c12c5da7f6b718535666e6e745bd
ubol12341 <ubol1234.1@gmail.com>
2025-07-24 23:02:15 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,1 @@
-am start -n com.termux/com.termux.app.TermuxActivity
-
-sleep 5s
-
 am start -n com.anydesk.anydeskandroid/com.anydesk.anydeskandroid.gui.activity.MainActivity
-
-sleep 5s
-
-am start -n com.termux/com.termux.app.TermuxActivity
