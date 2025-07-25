b16da892ab5c396da8785861a1c489378fc89bdb
ubol12341 <ubol1234.1@gmail.com>
2025-07-25 11:37:04 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 # am start -n com.anydesk.anydeskandroid/com.anydesk.anydeskandroid.gui.activity.MainActivity
-am start -n de.j4velin.wifiAutoOff/de.j4velin.wifiAutoOff.Preferences
+# am start -n de.j4velin.wifiAutoOff/de.j4velin.wifiAutoOff.Preferences
